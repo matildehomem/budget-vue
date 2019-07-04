54d1f522a274b50dd54bbbd8fae4ab5bcca9bf26
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2019-07-04 16:51:11 +0100

    pig image

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,74 @@
 <template>
   <div id="app">
-    <h1>Budget App - Vue</h1>
+    <header>
+      <div class="wrapper">
+        <div class="current-wrapper">
 
-    <div class="current-value">
-      Valor currente:
-      <span>{{current}}</span>
-    </div>
 
-    <div class="board-wrapper">
-      <div class="income">
-        <h2>Income</h2>
-        <div class="list">
-          <ul>
-            <li v-for="(income, index) in incomes" v-bind:key="income.id">
-              {{income.name}} -
-              <strong>{{income.value}} €</strong>
-              <button @click="removeItem(index,'incomes')">delete</button>
-            </li>
-          </ul>
+
+        <div class="pigImage" :class="pigImage"></div>
+      <div class="current-box">
+
+        <p>O meu dinheiro</p>
+        <div class="current-value">{{current}} €</div>
+      </div>
         </div>
-        <div class="total">
-          <p>
-            TOTAL:
-            <span>{{totalIncome}}</span>
-          </p>
+        <p>Indique aqui as suas receitas ou despesas</p>
+
+
+        <form>
+          <label for="name">Descrição</label>
+          <input type="text" v-model="newInputText" />
+          <label for="value">Euro</label>
+          <input type="number" v-model="newInputValue" required />
+          <label for="income">Income</label>
+          <input type="radio" name="type" value="income" v-model="incomeCheck" required />
+          <label for="expense">Expense</label>
+          <input type="radio" name="type" value="expense" v-model="expenseCheck" />
+          <input type="submit" value="submit" @click.prevent="calCurrent()" />
+        </form>
+      </div>
+    </header>
+    <section>
+      <div class="wrapper board-wrapper">
+        <div class="income">
+          <h2>Income</h2>
+          <div class="total">
+            <p>
+              TOTAL:
+              <span>{{totalIncome}} €</span>
+            </p>
+          </div>
+          <div class="list">
+            <ul>
+              <li v-for="(income, index) in incomes" :key="income.id">
+                {{income.name}} -
+                <strong>{{income.value}} €</strong>
+                <button @click="removeItem(index,'incomes')">delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="expenses">
+          <h2>Expenses</h2>
+          <div class="total">
+            <p>
+              TOTAL:
+              <span>{{totalExpenses}}</span>
+            </p>
+          </div>
+          <div class="list">
+            <ul>
+              <li v-for="(expense, index) in expenses" v-bind:key="expense.id">
+                {{expense.name}} -
+                <strong>{{expense.value}} €</strong>
+                <button @click="removeItem(index, 'expenses')">delete</button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="expenses">
-        <h2>Expenses</h2>
-        <div class="list">
-          <ul>
-            <li v-for="(expense, index) in expenses" v-bind:key="expense.id">
-              {{expense.name}} -
-              <strong>{{expense.value}} €</strong>
-              <button @click="removeItem(index, 'expenses')">delete</button>
-            </li>
-          </ul>
-        </div>
-        <div class="total">
-          <p>
-            TOTAL:
-            <span>{{totalExpenses}}</span>
-          </p>
-        </div>
-      </div>
-    </div>
-    <form>
-      <label for="name">Descrição</label>
-      <input type="text" v-model="newInputText" />
-      <label for="value">Euro</label>
-      <input type="number" v-model="newInputValue" />
-      <label for="income">Income</label>
-      <input type="checkbox" name="income" value="income" v-model="incomeCheck" />
-      <label for="expense">Expense</label>
-      <input type="checkbox" name="expense" value="expense" v-model="expenseCheck" />
-      <input type="submit" value="submit" @click.prevent="calCurrent" />
-    </form>
+    </section>
   </div>
 </template>
 
@@ -65,7 +78,6 @@ export default {
   name: "app",
   data() {
     return {
-      
       incomeCheck: false,
       expenseCheck: false,
 
@@ -98,12 +110,20 @@ export default {
       });
 
       return result;
-
-
     },
-   
+
     current() {
       return this.totalIncome - this.totalExpenses;
+    },
+    pigImage: function() {
+      if (this.current < 0 && this.current > -200) {
+        return "unhappy";
+      } else if (this.current > 200) {
+        return "happy";
+      } else if (this.current < -200) {
+        return "dead";
+      }
+      return "normal";
     }
   },
   methods: {
@@ -119,9 +139,7 @@ export default {
         this.newInputText = "";
         this.newInputValue = "";
         this.incomeCheck = false;
-
       }
-
       //if expense
       if (this.expenseCheck) {
         //add to income array
@@ -133,13 +151,11 @@ export default {
         this.newInputText = "";
         this.newInputValue = "";
         this.expenseCheck = false;
-
       }
     },
-     removeItem(index, list) {
+    removeItem(index, list) {
       this[list].splice(index, 1);
-     
-    },
+    }
   }
 };
 </script>
