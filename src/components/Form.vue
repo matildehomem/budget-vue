2983b05ae198d7746201459d49db5c5e5053a4a0
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2019-07-25 15:42:31 +0100

    passing methods

---
+++ src/components/Form.vue
@@ -0,0 +1,83 @@
+<template>
+  <form>
+    <div>
+      <div class="form-group">
+        <label for="name">Descrição</label>
+        <input type="text" v-model="newInputText" />
+      </div>
+
+      <div class="form-group">
+        <label for="value">Euro</label>
+        <input type="number" v-model="newInputValue" />
+      </div>
+
+      <div class="form-group">
+        <label for="income">Income</label>
+        <input type="radio" name="type" id="income" value="income" v-model="picked" />
+      </div>
+      <div class="form-group">
+        <label for="expense">Expense</label>
+        <input type="radio" name="type" id="expense" value="expense" v-model="picked" />
+      </div>
+      <p v-show="isValidationAllowed==false">Campos com erros</p>
+    </div>
+    <input
+      type="submit"
+      value="Submeter"
+      @click="validate"
+      @click.prevent="validate(), checkType()"
+    />
+  </form>
+</template>
+
+
+<script>
+export default {
+  props: ["incomes", "expenses"],
+  data() {
+    return {
+      picked: "income",
+      newInputText: "",
+      newInputValue: "",
+      nextId: 0,
+      isValidationAllowed: null
+    };
+  },
+  methods: {
+    validate() {
+      if (
+        this.newInputText == "" ||
+        this.newInputValue == "" ||
+        parseInt(this.newInputValue) <= 0
+      ) {
+        this.isValidationAllowed = false;
+      } else this.isValidationAllowed = true;
+    },
+    
+    checkType() {
+      //confirm is input is empty
+      if (this.isValidationAllowed == false) return;
+
+      //store data in object
+      let data = {
+        id: this.nextId++,
+        name: this.newInputText,
+        value: this.newInputValue
+      };
+
+      //push to correct array
+      this.picked == "income"
+        ? this.incomes.push(data)
+        : this.expenses.push(data);
+
+      this.$emit('saveDataInputDois');
+
+      //empty form
+      this.newInputText = "";
+      this.newInputValue = "";
+      this.picked = "income";
+      this.isValidationAllowed = null;
+    }
+  }
+};
+</script>
---
+++ src/components/SectionBoard.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="wrapper grey">
+    <div class="board-wrapper">
+      <incomes
+        :incomes="incomes"
+        @removeItem="removeItem"
+        @removeList="removeList"
+        @incomeResult="incomeResult"
+      ></incomes>
+      <expenses
+        :expenses="expenses"
+        @removeItem="removeItem"
+        @removeList="removeList"
+        @expensesResult="expensesResult"
+      ></expenses>
+    </div>
+    <button
+      class="app__btn"
+      v-if="incomes.length && expenses.length"
+      @click.prevent="$emit('removeAll', ['incomes','expenses'])"
+    >Delete both lists</button>
+  </section>
+</template>
+<script>
+import Incomes from "./Incomes";
+import Expenses from "./Expenses";
+export default {
+  components: {
+    incomes: Incomes,
+    expenses: Expenses
+  },
+  props: ["expenses", "incomes"],
+  methods: {
+    removeItem(index, list) {
+      this.$emit('removeItem', index, list);
+    },
+    removeList(list){
+      this.$emit('removeList', list);
+    },
+    incomeResult(result) {
+      this.$emit("incomeResult", result);
+    },
+    expensesResult(result) {
+      this.$emit("expensesResult", result);
+    }
+  }
+};
+</script>
+
